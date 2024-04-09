4be89a5a2d2b9f3223b48dbbcb7111e05ed4d565
IrviiingVZ <150636449+IrviiingVZ@users.noreply.github.com>
2024-04-09 16:51:12 -0600

    Add files via upload

---
+++ EVA2_18_FOR/src/main/java/com/mycompany/eva2_18_for/EVA2_18_FOR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_18_for;
+
+/**
+ *
+ * @author irvingvalles
+ */
+public class EVA2_18_FOR {
+
+    public static void main(String[] args) {
+        //INICIO, CONDICION, INCREMENTO/DECREMENTO
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + "--");
+        }
+        System.out.println("");{
+    }
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i + "--");
+        }
+        System.out.println("");{
+    }
+        for(int i = 0; i <= 100; i+=2){
+            System.out.println(i + "--");
+        
+        }
+    }
+}
