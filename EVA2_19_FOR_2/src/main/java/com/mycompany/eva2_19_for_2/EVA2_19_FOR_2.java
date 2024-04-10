da1dc2bb209fc92883a94b9b948cb21389368473
IrviiingVZ <150636449+IrviiingVZ@users.noreply.github.com>
2024-04-10 16:50:29 -0600

    Add files via upload

---
+++ EVA2_19_FOR_2/src/main/java/com/mycompany/eva2_19_for_2/EVA2_19_FOR_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_19_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author irvingvalles
+ */
+public class EVA2_19_FOR_2 {
+
+    public static void main(String[] args) {
+     int numAlum;
+     Scanner captu = new Scanner(System.in);
+     System.out.println("Cuantos estudiantes tiene le grupo?");
+        numAlum = captu.nextInt();
+        //Hay que capturar todaslas calificaciones:
+        //Los dos for hacen lo mismo
+        for (int i = 1; i <= numAlum; i++){
+            System.out.println("Califcacion: ");
+            int califa = captu.nextInt();
+          
+            //i = 0 
+     }
+        
+    }
+}
